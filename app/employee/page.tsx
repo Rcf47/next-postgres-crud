9bca84aa61e3505d377923e2b72695599d214ea0
Vadim Cherepanov <xenos26@gmail.com>
2024-06-06 21:42:09 +0700

    feat: start working on page employee

---
+++ app/employee/page.tsx
@@ -0,0 +1,14 @@
+const Employee = async () => {
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <h1 className="text-4xl font-bol">
+          Next js 14 CRUD Create Read Update Delete <br />
+          Prisma PostgreSQL | Tailwind DaisyUI
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Employee;
